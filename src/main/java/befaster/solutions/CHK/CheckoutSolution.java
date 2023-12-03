f8c1c0bb66ea74eab87fbaef918e82566cd13fec
sbx_user1051 <sbx_user1051@169.254.63.213>
2023-12-03 16:33:11 +0000

    Sun Dec 03 16:33:11 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,11 +2,39 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+
 public class CheckoutSolution {
+
+    // Map of String (SKU) -> Value (Pound)
+    private HashMap<String, Integer> products = new HashMap<>();
+    private HashMap<String, Integer> specialOffers = new HashMap<>();
+
+    public CheckoutSolution() {
+        init();
+    }
+
+    private void init() {
+        products.put("A", 50);
+        products.put("B", 30);
+        products.put("C", 20);
+        products.put("D", 15);
+
+        specialOffers.put("3A", 130);
+        specialOffers.put("2B", 45);
+    }
+
     public Integer checkout(String skus) {
-        if (skus == null || skus.isBlank() || skus.) {
+        // Returns -1 if:
+        // String is null
+        // String is blank or empty
+        // String has digits
+        if (skus == null || skus.isBlank() || skus.matches(".*\\d.*")) {
             return -1;
         }
+
+        skus.split
     }
 }
+
 
