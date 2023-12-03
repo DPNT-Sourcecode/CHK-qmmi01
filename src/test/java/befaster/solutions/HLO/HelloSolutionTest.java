8a41c89f5117f5fac536c090c6fd495edebc3d54
sbx_user1051 <sbx_user1051@169.254.63.213>
2023-12-03 16:21:10 +0000

    Sun Dec 03 16:21:10 UTC 2023

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,2 +1,22 @@
-package befaster.solutions;public class HelloSolutionTest {
+package befaster.solutions.HLO;
+
+import befaster.solutions.SUM.SumSolution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloSolutionTest {
+    private HelloSolution hlo;
+
+    @BeforeEach
+    public void setUp() {
+        hlo = new HelloSolution();
+    }
+
+    @Test
+    public void helloWorldNullFriendNameTest(){
+        assertEquals("Hello World!", hlo.hello(null));
+    }
 }
+
